2a284cbf1bf121ec6d2357c98cf6372fbcff3917
ShuChao <1224881520@qq.com>
2021-11-11 10:19:31 +0800

    first commit

---
+++ src/main/java/git/GitTest.java
@@ -0,0 +1,11 @@
+package git;
+
+/**
+ * @author shkstart
+ * @create 2021-11-11-10:14
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
